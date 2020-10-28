211288508ed6265282872e3b760d872b0c34a5a8
Dana Bliss <danajackson2@gmail.com>
2020-10-28 17:31:58 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,18 @@
 def find_even_values(src)
+  i = 0
+  while i < src.length do
+    ii = 0
+    while ii < src[i].length do
+      src[i][ii] % 2 == 0
+        puts src[i][ii]
+      ii += 1
+    end
+    i += 1
+  end
+end
+
+
+def find_even_values(src)
   src.flatten.each {|num|
   if num.even?
     puts num
